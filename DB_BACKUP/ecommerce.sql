1adec6d3e4a3f8f431b99024ee38ae33efb576b0
Md.Shamim Haque <jahangiralomshamim3@gmail.com>
2021-06-02 09:47:17 +0600

    'update'

--- DB_BACKUP/ecommerce.sql
+++ DB_BACKUP/ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2021 at 06:40 AM
+-- Generation Time: Jun 02, 2021 at 05:46 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
